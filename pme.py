0cba10dd8bd8b510bf4befbbf944413c07f51424
Jenny Xing <jennyxing27@gmail.com>
2016-06-15 11:47:51 -0400

    Removed old code
    old URLs and templates are removed, and webglTiles is renamed to pme.

--- pme.py
+++ pme.py
@@ -1,27 +1,16 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-from pointmatchloading import getTileData, getPointMatches
+from pointmatchloading import getTileData
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/viewboundingbox')
-def render():
-    bounds = getTileData(501, 501)
-    return render_template('pme.html', bounds = bounds)
-
 @app.route('/')
-def threejstest():
-    tiledata = getTileData(3722, 3722)
-    return render_template('3dgridview.html', tiledata = tiledata)
-
-@app.route('/webgltesting')
-def webgltesting():
+def render():
     # tiledata = getTileData(1, 7062, samplingRate = 50)
-    tiledata = getTileData(500, 505)
-    # getPointMatches(1, 7000)
-    return render_template('webglTiles.html', tiledata = tiledata)
+    tiledata = getTileData(4, 6)
+    return render_template('pme.html', tiledata = tiledata)
 
 if __name__ == '__main__':
     app.run()
