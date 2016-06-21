39b905b0fdda4caa6e74b94e7557f8005baa7e62
Jenny Xing <jennyxing27@gmail.com>
2016-06-21 15:19:33 -0400

    fixed alignment issue
    getStackBounds is renamed to getSectionBounds
    code attempting to translate khaled’s matlab code is removed

--- pointmatchloading.py
+++ pointmatchloading.py
@@ -1,5 +1,6 @@
 from flask import current_app as app
 import requests
+import sys
 
 def getJSONfromURL(url):
     headers = {
@@ -12,7 +13,6 @@ def getJSONfromURL(url):
 def getSectionData():
     url = "/owner/{}/project/{}/stack/{}/sectionData".format(app.config["OWNER"], app.config["PROJECT"], app.config["STACK"])
     return getJSONfromURL(url)
-
 
 #gets specs for all tiles in a z layer
 def getTileSpecs(z):
@@ -30,7 +30,7 @@ def getTileSpec(tileId):
     return getJSONfromURL(url)
 
 #get bounds for a single stack
-def getStackBounds(z):
+def getSectionBounds(z):
     url = "/owner/{}/project/{}/stack/{}/z/{}/bounds".format(app.config["OWNER"], app.config["PROJECT"], app.config["STACK"], z)
     return getJSONfromURL(url)
 
@@ -43,19 +43,7 @@ def getMatchesOutsideGroup(groupId):
     url = "/owner/{}/matchCollection/{}/group/{}/matchesOutsideGroup".format(app.config["OWNER"], app.config["MATCH_COLLECTION"], groupId)
     return getJSONfromURL(url)
 
-#nfirst and nlast are zvalue of sections in rc
-#rc and pm are objects with specification for accessing collections of tile-specs and point-patches (required for the API)
-#nbr is the number of neighboring sections to consider
-#min_points is the minimum number of points between two tiles
-#xs_weight is the weight factor for cross-section point matches
-#yet to be written, translated from Khaled's MATLAB code
-def load_point_matches(nfirst, nlast, nbr = 4, min_points = 0, xs_weight = 1, samplingRate = 1):
-    sectionData = getSectionData()
-    sectionIDs = [val['sectionId'] for val in sectionData if val['z'] >= nfirst and val['z'] <= nlast]
-    zs = [val['z'] for val in sectionData if val['z'] >= nfirst and val['z'] <= nlast]
-    uniqueZs = list(set(zs))
-    return None
-
+#TODO not sure if this doing the right thing.
 def getUniqueZs(nfirst, nlast):
     sectionData = getSectionData()
     zs = [val['z'] for val in sectionData if val['z'] >= nfirst and val['z'] <= nlast]
@@ -63,15 +51,28 @@ def getUniqueZs(nfirst, nlast):
     uniqueZs.sort()
     return uniqueZs
 
-def getTileCoordinates(z):
+#returns (x,y) that indicates how much the X and Y should be translated to center all of the sections around (0,0)
+def calculateTranslation(nfirst, nlast):
+    minX = sys.maxint
+    minY = sys.maxint
+    maxX = 0
+    maxY = 0
+    for z in getUniqueZs(nfirst, nlast):
+        sectionBounds = getSectionBounds(z)
+        minX = min(minX, sectionBounds['minX'])
+        minY = min(minY, sectionBounds['minY'])
+        maxX = max(maxX, sectionBounds['maxX'])
+        maxY = max(maxY, sectionBounds['maxY'])
+    #calculates the center of the bounds of all of the sections combined
+    return (0.5*(minX+maxX), 0.5*(minY+maxY))
+
+def getTileCoordinates(z, translation):
     tileBounds = getTileBounds(z)
-    stackBounds = getStackBounds(z)
     for tile in tileBounds:
-        #translate the coordinates to center around (0,0)
-        tile['minX'] = tile['minX'] - 0.5 * (stackBounds['minX'] + stackBounds['maxX'])
-        tile['minY'] = tile['minY'] - 0.5 * (stackBounds['minY'] + stackBounds['maxY'])
-        tile['maxX'] = tile['maxX'] - 0.5 * (stackBounds['minX'] + stackBounds['maxX'])
-        tile['maxY'] = tile['maxY'] - 0.5 * (stackBounds['minY'] + stackBounds['maxY'])
+        tile['minX'] = tile['minX'] - translation[0]
+        tile['minY'] = tile['minY'] - translation[1]
+        tile['maxX'] = tile['maxX'] - translation[0]
+        tile['maxY'] = tile['maxY'] - translation[1]
     return tileBounds
 
 def getPointMatches(z):
@@ -84,11 +85,12 @@ def getPointMatches(z):
 #sampling rate limits the number of layers when the whole stack is generated
 def getTileData(nfirst, nlast, samplingRate = 1):
     tileData = []
+    translation = calculateTranslation(nfirst, nlast)
     for z in getUniqueZs(nfirst, nlast):
         if ( z % samplingRate == 0 ) :
             layerData = {}
             layerData['z'] = str(z)
-            layerData['tileCoordinates'] = getTileCoordinates(z)
+            layerData['tileCoordinates'] = getTileCoordinates(z, translation)
             layerData['pointMatches'] = getPointMatches(z)
             tileData.append(layerData)
     return tileData
