5bb3cc3762425e644bae2707aaaf11434082a015
Alex Weston <westona@janelia.hhmi.org>
2017-03-03 09:22:20 -0500

    Let url params and store vals have different names
    provides a mapping utility to allow url params and
    store vals to differ

--- src/components/UrlParamHandler.js
+++ src/components/UrlParamHandler.js
@@ -1,11 +1,10 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
 import {updateUserInput} from "../actions"
-import {userInputInitialState} from "../reducers"
 import URLSearchParams from "url-search-params"
-/*
-  Keeps url query params in sync with Redux store
-*/
+/**
+ * Keeps url query params in sync with Redux store
+ */
 class UrlParamHandler extends Component {
 
   constructor(props){
@@ -17,10 +16,11 @@ class UrlParamHandler extends Component {
       const paramTracker = new URLSearchParams(window.location.search)
       let inputData = {}
       for(let [key, val] of paramTracker.entries()){
-        if(userInputInitialState.hasOwnProperty(key)){
+        if(this.props.StoreData.hasOwnProperty(key)){
           inputData[key] = val
         }
       }
+
       //update store
       if(inputData){
         this.props.updateUserInput(inputData)
@@ -32,7 +32,7 @@ class UrlParamHandler extends Component {
     //update the url params
     const paramTracker = new URLSearchParams(window.location.search)
 
-    _.each(nextProps.UserInput, function(val,key){
+    _.each(nextProps.StoreData, function(val,key){
       paramTracker.set(key,val)
     })
     this.updateUrl(paramTracker)
@@ -48,18 +48,61 @@ class UrlParamHandler extends Component {
   }
 }
 
+/**
+ * Utility class for translating between url params and
+ * Store data, as these names may change independently
+ */
+class urlParamStoreMapper{
+
+  constructor(paramsToStoreMap){
+    this.urlParamsToStoreVals = paramsToStoreMap
+    this.storeValsToUrlParams = _.invert(this.urlParamsToStoreVals)
+  }
+
+  mapStoreDataToUrlParams(storeData){
+    let paramData = {}
+    for(let storeKey in storeData){
+      if(this.storeValsToUrlParams.hasOwnProperty(storeKey)){
+        paramData[this.storeValsToUrlParams[storeKey]] = storeData[storeKey]
+      }
+    }
+    return paramData
+  }
+
+  mapUrlParamsToStoreData(paramData){
+    let storeData = {}
+    for(let paramKey in paramData){
+      if(this.urlParamsToStoreVals.hasOwnProperty(paramKey)){
+        storeData[this.urlParamsToStoreVals[paramKey]] = paramData[paramKey]
+      }
+    }
+    return storeData
+  }
+}
+
+//create mappings between url params and store variables
+let userInputUrlMap = new urlParamStoreMapper({
+  renderStackOwner: "selectedStackOwner",
+  renderStackProject: "selectedProject",
+  renderStack: "selectedStack",
+  matchOwner: "selectedMatchOwner",
+  matchCollection: "selectedMatchCollection"
+})
+
 const mapStateToProps = function(state) {
   const {UserInput} = state
   return {
-    UserInput
+    StoreData: userInputUrlMap.mapStoreDataToUrlParams(UserInput)
   }
 }
 
 const mapDispatchToProps = function(dispatch) {
   return {
-    updateUserInput: function(userInputData){
+    updateUserInput: function(data){
+      let userInputData = userInputUrlMap.mapUrlParamsToStoreData(data)
       dispatch(updateUserInput(userInputData))
     },
   }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(UrlParamHandler)
